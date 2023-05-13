328cc9413d69e8cb296be61eb16e91f5489949e1
xlnt415 <wooaaa1005@hanyang.ac.kr>
2023-05-13 15:30:45 +0900

    조건문 남발
    반복문을 사용해볼 것

--- 코테 연습/그리디 알고리즘/거스름돈2.py
+++ 코테 연습/그리디 알고리즘/거스름돈2.py
@@ -17,17 +17,21 @@ if money < 10:
             print(money // 2)
 
         else:
-            print(1 + ((money - 5) // 2))
+            print(1 + ((money - 5) // 2)) # 완료
 
 # 10보다 큰 경우
-elif money % 5 == 0 or money % 2 == 0:
-    m_5, m_2 = money // 5, money % 5 // 2
-
-    print(m_5 + m_2)
 
 else:
-    m_5, m_2 = (money // 5) - 1, (money % 5 + 5) // 2
+    if money % 5 == 0:
+        print(money // 5)
 
-    print(m_5 + m_2)
+    elif money % 5 % 2 == 0:
+        m_5, m_2 = money // 5, money % 5 // 2
+        print(m_5 + m_2)
 
-# 17을 넣어보니 틀림....
+    else:
+        # ex) 26
+        m_5, m_2 = ((money // 10) * 10) // 5,  ((money % 10)
+        print(m_5 + m_2)
+# 17을 넣어보니 틀림....(해결)
+# 26을 넣어보니 틀림....
