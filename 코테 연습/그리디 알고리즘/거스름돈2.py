52988ff8b72d660e699fa7cbb059e1d5007abeca
Woo <97427612+xlnt415@users.noreply.github.com>
2023-05-13 14:52:45 +0000

    그리디 알고리즘

--- 코테 연습/그리디 알고리즘/거스름돈2.py
+++ 코테 연습/그리디 알고리즘/거스름돈2.py
@@ -1,37 +1,28 @@
 money = int(input("거스름 돈 입력 : "))
 
 # 10보다 작은 경우
-if money < 10:
-    if money < 5:
-        if money % 2 == 0:
-            print(money // 2)
 
-        else:
-            print(-1)
+coin_types = [5, 2]
+count = 0
 
-    else:
-        if money % 5 == 0:
-            print(1)
 
-        elif money % 2 == 0:
-            print(money // 2)
+for coin in coin_types:
+    # -1이 출력되는 경우 = 1, 3
+    if money % [i for i in range(2, 10) != 0]:
+         count += money // coin
+         money %= coin
+         # 소수일 경우 생각하기
+         
+        
+        
+    
+    
+    else:    
+        count += money // coin
+        money %= coin
 
-        else:
-            print(1 + ((money - 5) // 2)) # 완료
 
-# 10보다 큰 경우
-
-else:
-    if money % 5 == 0:
-        print(money // 5)
-
-    elif money % 5 % 2 == 0:
-        m_5, m_2 = money // 5, money % 5 // 2
-        print(m_5 + m_2)
-
-    else:
-        # ex) 26
-        m_5, m_2 = ((money // 10) * 10) // 5,  ((money % 10)
-        print(m_5 + m_2)
+    
+print(count)
 # 17을 넣어보니 틀림....(해결)
 # 26을 넣어보니 틀림....
